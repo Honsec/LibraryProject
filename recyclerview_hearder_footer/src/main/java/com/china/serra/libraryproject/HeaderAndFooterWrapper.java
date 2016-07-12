406a20f13ed5ed63191d79cc83be975787882e26
Hongsec <hongsec@miniram.com>
2016-07-12 15:31:52 +0900

    Initial commit

---
+++ recyclerview_hearder_footer/src/main/java/com/china/serra/libraryproject/HeaderAndFooterWrapper.java
@@ -0,0 +1,24 @@
+package com.china.serra.libraryproject;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by Hongsec on 2016-07-12.
+ */
+public class HeaderAndFooterWrapper<T> extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
